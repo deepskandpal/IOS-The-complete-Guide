1deedf3ec2c1e3551627e25577155dae216df245
Deepanshu Kandpal <dkandpalz@gmail.com>
2019-01-26 21:05:46 -0800

    helloWorld UI/UX : Playing with labels

---
+++ Swift/UI UX/HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by mac on 1/26/19.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
