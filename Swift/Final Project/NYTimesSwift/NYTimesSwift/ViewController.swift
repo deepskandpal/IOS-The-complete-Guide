93dfcb82d0636152d0520e98e4841060bcb0bab3
Deepanshu Kandpal <dkandpalz@gmail.com>
2019-02-13 17:25:57 +0530

    NyTimes project

--- Swift/Final Project/NYTimesSwift/NYTimesSwift/ViewController.swift
+++ Swift/Final Project/NYTimesSwift/NYTimesSwift/ViewController.swift
@@ -8,13 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    
+    
+   
 
+    @IBOutlet weak var NyTimesMainView: UITableView!{
+        didSet{
+            NyTimesMainView.delegate = self
+            NyTimesMainView.dataSource = self
+            NyTimesMainView.register(UINib(nibName: "NyTimesTableCell", bundle: nil), forCellReuseIdentifier: "NyTimesCell")
+            }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
 }
 
